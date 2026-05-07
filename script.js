697ef85bd15452ecddb2cc7eece1cf3e130bc074
Axel-ne <axel-neugebauer@gmx.net>
2026-05-07 15:35:40 +0200

    img hover

--- script.js
+++ script.js
@@ -8,4 +8,30 @@
 //     dialogRef.close();
 // }
 
-const imgsarr = [];
+
+
+
+const imgs = [
+    "assets/imgs/Component 6.png",
+    "assets/imgs/Component 7.png",
+    "assets/imgs/Component 8.png",
+    "assets/imgs/Component 9.png",
+    "assets/imgs/Component 10.png",
+    "assets/imgs/Component 11.png",
+    "assets/imgs/Component 12.png",
+    "assets/imgs/Component 13.png",
+    "assets/imgs/Component 14.png",
+    "assets/imgs/Component 15.png",
+    "assets/imgs/Component 16.png",
+    "assets/imgs/gallery.png"
+];
+
+const container = document.getElementById("container");
+
+function renderImgs() {
+    for (let i = 0; i < imgs.length; i++) {
+        container.innerHTML += `<img class="imgs-content" src="${imgs[i]}">`
+    }
+}
+
+renderImgs();
