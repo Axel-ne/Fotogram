3a06d9defe23c97f85b82fca037a4caee18d0af4
Axel-ne <axel-neugebauer@gmx.net>
2026-05-09 17:38:35 +0200

    dialog done

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const dialogRef = document.getElementById("myDialog");
 
-function openDialog() {
+const dialogRendeer = document.getElementById("dialog-render");
+
+const imgCounter = document.getElementById("img-counter");
+
+const imgText = document.getElementById("img-text");
+
+let currentIndex = 0;
+
+function openDialog(j) {
+    currentIndex = j;
+    dialogRendeer.innerHTML = `<img  src="${imgs[j]}">`;
     dialogRef.showModal();
+    imgText.innerText = imgs[j];
+    updateCounter();
 }
 
 function closeDialog() {
@@ -9,37 +21,50 @@ function closeDialog() {
 }
 
 const imgs = [
-    "assets/imgs/Component 6.png",
-    "assets/imgs/Component 7.png",
-    "assets/imgs/Component 8.png",
-    "assets/imgs/Component 9.png",
-    "assets/imgs/Component 10.png",
-    "assets/imgs/Component 11.png",
-    "assets/imgs/Component 12.png",
-    "assets/imgs/Component 13.png",
-    "assets/imgs/Component 14.png",
-    "assets/imgs/Component 15.png",
-    "assets/imgs/Component 16.png",
-    "assets/imgs/gallery.png"
+    "./assets/imgs/Component 6.png",
+    "./assets/imgs/Component 7.png",
+    "./assets/imgs/Component 8.png",
+    "./assets/imgs/Component 9.png",
+    "./assets/imgs/Component 10.png",
+    "./assets/imgs/Component 11.png",
+    "./assets/imgs/Component 12.png",
+    "./assets/imgs/Component 13.png",
+    "./assets/imgs/Component 14.png",
+    "./assets/imgs/Component 15.png",
+    "./assets/imgs/Component 16.png",
+    "./assets/imgs/gallery.png",
 ];
 
 const container = document.getElementById("container");
 
 function renderImgs() {
     for (let i = 0; i < imgs.length; i++) {
-        container.innerHTML += `<img onclick="openDialog()" class="imgs-content" src="${imgs[i]}">`;
+        container.innerHTML += `<img onclick="openDialog(${i})" class="imgs-content" src="${imgs[i]}">`;
     }
 }
 
-renderImgs();
+function nextImg() {
+    currentIndex++;
+    if (currentIndex >= imgs.length) {
+        currentIndex = 0;
+    }
 
-// function renderDialog() {
-//     const dialog = `/*html*/
-//         <dialog>
-//         <header>
-//         <h2></h2>
-//         </header>
-//         <section></section>
-//         </dialog>
-//     `
-// }
+    dialogRendeer.innerHTML = `<img src="${imgs[currentIndex]}">`;
+    imgText.innerText = imgs[currentIndex];
+    updateCounter();
+}
+
+function previusImg() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = imgs.length - 1;
+    }
+
+    dialogRendeer.innerHTML = `<img src="${imgs[currentIndex]}">`;
+    imgText.innerText = imgs[currentIndex];
+    updateCounter();
+}
+
+function updateCounter() {
+    imgCounter.innerText = `${currentIndex + 1}/${imgs.length}`;
+}
